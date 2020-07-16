4c9ae429424ce56407c4814fa106622b315e8a1b
Petya Buchkova <petya.buchkova@trafficlab.dk>
2020-07-16 13:41:39 +0200

    Added shuffle function

--- deck.go
+++ deck.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"time"
 	"fmt"
 	"strings"
- "io/ioutil"
+	"io/ioutil"
+	"os"
+	"math/rand"
 )
 
 type deck []string
@@ -47,4 +50,26 @@ func (d deck) toString() string {
 func (d deck) saveToFile(filename string) error {
 	// 0666 is default permissions on the file - anyone can read and write
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	// if no err, err will be nil (=null)
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+			fmt.Println("Error:", err)
+			// any argument beside 0 indicates sth went wrong
+			os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",") //slice of strings
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d)-1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 func main() {
-	cards := newDeck()
-	// cards.print()
-	// hand, remainingDeck := deal(cards, 5)
-	// hand.print()
-	// remainingDeck.print()
-	cards.saveToFile("my_cards")
-
+	cards := newDeckFromFile("my_cards")
+	cards.shuffle()
+	cards.print()
 }
 
